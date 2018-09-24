2c0d30b067ae920ea933746cb8561aa26ff3c1cc
Andrew Pla <32366464+AndrewPla@users.noreply.github.com>
2018-09-23 22:21:32 -0400

    added get-tdapiversion

---
+++ TOPdeskPS/functions/Get-TdApiVersion.ps1
@@ -0,0 +1,24 @@
+function Get-TdApiVersion {
+    <#
+    .SYNOPSIS
+        Gets version of the TOPdesk API
+    .DESCRIPTION
+        Gets version of the TOPdesk API
+    .EXAMPLE
+        PS C:\> Get-TdApiVersion
+        Gets version of the TOPdesk API
+
+    #>
+    [CmdletBinding()]
+    param (
+        
+    )
+    Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug', 'start', 'param'
+    $uri = (Get-TdUrl) + '/tas/api/version'
+    Write-PSFMessage -Level InternalComment -Message "version url: $uri"
+    $Params = @{
+        'uri' = $uri
+    }
+    $res = Invoke-TdMethod @Params
+    $res
+}
