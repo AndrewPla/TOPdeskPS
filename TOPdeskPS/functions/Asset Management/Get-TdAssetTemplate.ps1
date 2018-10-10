091e218de8ba47af8a500abf6152e4e44a1105fe
Andrew Pla <32366464+AndrewPla@users.noreply.github.com>
2018-10-10 13:15:47 -0400

    moved around parameters

--- TOPdeskPS/functions/Asset Management/Get-TdAssetTemplate.ps1
+++ TOPdeskPS/functions/Asset Management/Get-TdAssetTemplate.ps1
@@ -15,11 +15,12 @@ function Get-TdAssetTemplate {
     #>
     [CmdletBinding(HelpUri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdAssetTemplate')]
     param (
-        [switch]
-        $Archived,
-
+        [Parameter(Position = 0)]
         [System.String]
-        $Name = '*'
+        $Name = '*',
+
+        [switch]
+        $Archived
     )
     Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug', 'start', 'param'
     $uri = (Get-TdUrl) + '/tas/api/assetmgmt/templates'
