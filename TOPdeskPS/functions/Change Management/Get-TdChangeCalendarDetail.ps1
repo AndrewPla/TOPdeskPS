10b47d9b06e46045202f3f723caece8c9fe1de29
Michael Arena <mxiia@mxiia.com>
2018-10-23 15:01:19 -0400

    Get-TdChangeCalendarDetail

---
+++ TOPdeskPS/functions/Change Management/Get-TdChangeCalendarDetail.ps1
@@ -0,0 +1,40 @@
+function Get-TdChangeCalendarDetail {
+    <#
+.SYNOPSIS
+    returns detailed information about the change
+.DESCRIPTION
+    returns detailed infromation about the change
+.Parameter CalendarId
+    Id of the calendar event
+.EXAMPLE
+    PS C:\> Get-TdChangeCalendarDetail -ChangeId $changeId
+    returns detailed information about the change
+#>
+    [CmdletBinding(HelpUri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdChangeCalendarDetail')]
+
+    param
+    (
+        [Parameter(
+            mandatory = $true,
+            ValueFromPipelineByPropertyName
+        )]
+        [Alias('Id')]
+        $CalendarId
+    )
+    begin {
+        Write-PsfMessage "[$($MyInvocation.MyCommand.Name)] Function started" -level verbose
+    }
+
+    process {
+        Write-PsfMessage "ParameterSetName: $($PsCmdlet.ParameterSetName)" -level InternalComment
+        Write-PSfMessage "PSBoundParameters: $($PSBoundParameters | Out-String)" -level InternalComment
+
+        $uri = "$(Get-TdUrl)/tas/api/changeCalendar/$CalendarId"
+        $res = Invoke-TdMethod -Uri $uri
+        $res
+    }
+    end {
+        Write-PSFMessage "Function Complete" -level Verbose
+    }
+}
+
