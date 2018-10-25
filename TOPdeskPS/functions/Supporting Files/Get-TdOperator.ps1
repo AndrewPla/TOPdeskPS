0c31b8ef2206dbfd3ddd92cf77d1e12a5894a78f
Michael Arena <mxiia@mxiia.com>
2018-10-25 09:56:42 -0400

    Added -name filter to Get-TdOperator

--- TOPdeskPS/functions/Supporting Files/Get-TdOperator.ps1
+++ TOPdeskPS/functions/Supporting Files/Get-TdOperator.ps1
@@ -4,19 +4,22 @@
     returns list of operators
 .DESCRIPTION
     returns list of operators
+.PARAMETER Name
+    human readable name to filter for operator by. Uses the dynamcName field
 .EXAMPLE
     PS C:\> Get-TdOperator
     returns list of operators
 #>
-[CmdletBinding(HelpUri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdOperator')]
+    [CmdletBinding(HelpUri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdOperator')]
 
     param
     (
-
+        [string]
+        $Name
     )
     begin {
         Write-PsfMessage "[$($MyInvocation.MyCommand.Name)] Function started" -level verbose
-        }
+    }
 
     process {
         Write-PsfMessage "ParameterSetName: $($PsCmdlet.ParameterSetName)" -level internalcomment
@@ -24,7 +27,12 @@
 
         $uri = (Get-TdUrl) + "/tas/api/operators"
         $res = Invoke-TdMethod -Uri $uri
-        $res
+        if ($name) {
+            $res | Where-Object dynamicName -like $Name
+        }
+        else {
+            $res
+        }
     }
     end {
         Write-PSFMessage "Function Complete" -level verbose
