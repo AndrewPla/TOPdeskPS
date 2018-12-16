3749c5d2f50f6f21fb51ae6792464cddca6326e4
Andrew Pla <32366464+AndrewPla@users.noreply.github.com>
2018-12-16 12:58:53 -0500

    Updated Name parameter

--- TOPdeskPS/functions/General API/Get-TdCallType.ps1
+++ TOPdeskPS/functions/General API/Get-TdCallType.ps1
@@ -14,7 +14,7 @@
     [CmdletBinding(Helpuri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdCallType')]
     param (
         [system.string]
-        $Name
+        $Name = '*'
     )
     Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug', 'start', 'param'
     $uri = (Get-TdUrl) + '/tas/api/incidents/call_types'
--- TOPdeskPS/functions/General API/Get-TdDuration.ps1
+++ TOPdeskPS/functions/General API/Get-TdDuration.ps1
@@ -14,7 +14,7 @@
     #>
     [CmdletBinding(Helpuri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdDuration')]
     param (
-        [system.string]$Name
+        [system.string]$Name = '*'
     )
     Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug', 'start', 'param'
     $uri = (Get-TdUrl) + '/tas/api/incidents/durations'
