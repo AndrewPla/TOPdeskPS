8574a427040350a0968a402664ac00f65e5c450c
Andrew Pla <32366464+AndrewPla@users.noreply.github.com>
2018-12-16 17:26:48 -0500

    added name filter parameter

--- TOPdeskPS/functions/Supporting Files/Get-TdBranchFilter.ps1
+++ TOPdeskPS/functions/Supporting Files/Get-TdBranchFilter.ps1
@@ -4,14 +4,18 @@
         Get list of branch filters
     .DESCRIPTION
         Get list of branch filters
+    .PARAMETER Name
+        Filter on the name. Wildcards supported.
     .EXAMPLE
         PS C:\> Get-TdBranchFilter
         Gets list of branch filters
     #>
     [CmdletBinding(HelpUri = 'https://andrewpla.github.io/TOPdeskPS/commands/Get-TdBranchFilter')]
-    param ()
+    param (
+        $Name = '*'
+    )
     Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ", ")" -Tag 'debug', 'start', 'param'
     $uri = (Get-TdUrl) + '/tas/api/operators/filters/branch'
-        $res = Invoke-TdMethod -Uri $uri
-        $res
+    $res = Invoke-TdMethod -Uri $uri
+    $res | where name -like $Name
 }
