e9cf94e55a150bfd836a31785448e7ac80d9eca1
AndrewPla <32366464+AndrewPla@users.noreply.github.com>
2019-02-02 10:20:22 -0500

    moved build files to build folder

---
+++ build/build.ps1
@@ -0,0 +1,22 @@
+﻿param([string[]]$Task = 'Default', $apikey)
+
+# Grab nuget bits, install modules, set build variables, start build.
+#Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+Install-Module Psake, PSDeploy, Pester, BuildHelpers, psframework, psscriptanalyzer, platyps -force
+Import-Module Psake, BuildHelpers
+
+Set-BuildEnvironment -Force
+
+$psakeParams = @{
+    buildfile = '.\psake.ps1'
+    tasklist = $task
+    nologo = $true
+    Verbose = $VerbosePreference
+}
+
+if ($apikey) { $psakeParams['parameters'] = @{psgallery = $apikey} 
+}
+
+Invoke-psake @psakeParams
+exit ( [int]( -not $psake.build_success ) )
