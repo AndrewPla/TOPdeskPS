67d75e96667b01873fccf0e64f71d5505a6f77c3
AndrewPla <32366464+AndrewPla@users.noreply.github.com>
2019-02-02 20:04:55 -0500

    added resolve-path to a line that keeps failing

--- build/psake.ps1
+++ build/psake.ps1
@@ -175,7 +175,7 @@ Task Compile -depends Test {
 
 
     # Gather commands
-    Get-ChildItem -Path "$($publishDir.FullName)\$env:BHProjectName\internal\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
+    Get-ChildItem -Path (Resolve-Path "$($publishDir.FullName)\$env:BHProjectName\internal\functions").path -Recurse -File -Filter "*.ps1" | ForEach-Object {
         $text += [System.IO.File]::ReadAllText($_.FullName)
     }
     Get-ChildItem -Path "$($publishDir.FullName)\$env:BHProjectName\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
